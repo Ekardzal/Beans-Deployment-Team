e114e05e53c53e0ad21645fbadf405c2c0f4b126
milaxxmila <166139745+milaxxmila@users.noreply.github.com>
2025-04-25 14:16:52 +0200

    Create InferenceTesting.py

---
+++ Tristan/InferenceTesting.py
@@ -0,0 +1,42 @@
+from ultralytics import YOLO
+import os
+import csv
+import shutil
+from colorama import Fore, Back, Style
+
+#region Variables
+_inputModelPath = r"F:\Unii\AAAAAAA\best.pt"
+_inputTestImagesPath = r"F:\Unii\test\imgstore" 
+
+_outputPredictionResultsPath = r"F:\Unii\GitHub\Beans-Deployment-Team\Tristan\output"
+
+
+
+#endregion
+
+#region Functions
+
+#endregion
+
+# Main
+model = YOLO(_inputModelPath)
+
+results = model.predict(_inputTestImagesPath, save=True, project=_outputPredictionResultsPath)
+
+rawCSVData = []
+
+for result in results: 
+    _imageName = os.path.basename(result.path)
+    for box in result.boxes.cpu().numpy():
+        _confidence = str(box.conf[0]).replace(".", ",")
+        _class = model.names[int(box.cls[0])]
+        print(_imageName + _class + _confidence)
+
+        rawCSVData.append((_imageName, _confidence, _class))
+
+csv_path = os.path.join(_outputPredictionResultsPath, "results.csv")
+
+with open(csv_path, mode="w", newline="") as file:
+    writer = csv.writer(file, delimiter=";")
+    writer.writerow(["File Name", "Confidence", "Class"])
+    writer.writerows(rawCSVData)
