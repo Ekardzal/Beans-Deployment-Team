d3b805e5cbd3623d9f3593f2b87407ac9204523b
milaxxmila <166139745+milaxxmila@users.noreply.github.com>
2025-05-09 10:37:58 +0200

    Update InferenceTesting.py

--- Tristan/InferenceTesting.py
+++ Tristan/InferenceTesting.py
@@ -2,10 +2,11 @@ from ultralytics import YOLO
 import os
 import csv
 from colorama import Fore, Back, Style
+import base64
 
 #region Variables
-_inputModelPath = r"F:\Unii\AAAAAAA\best.pt"
-_inputTestImagesPath = r"F:\Unii\GitHub\Beans-Deployment-Team\Tristan\input" 
+_inputModelPath = r"F:\Unii\GitHub\Beans-Deployment-Team\Tristan\best.pt"
+_inputTestImagesPath = r"F:\Unii\GitHub\Beans-Deployment-Team\Tristan\input_small" 
 
 _outputPredictionResultsPath = r"F:\Unii\GitHub\Beans-Deployment-Team\Tristan\output"
 #endregion
@@ -14,7 +15,7 @@ def SaveToCSV(_outputPredictionResultsPath, _rawCSVData):
     csv_path = os.path.join(_outputPredictionResultsPath, "results.csv")
     with open(csv_path, mode="w", newline="") as file:
         writer = csv.writer(file, delimiter=";")
-        writer.writerow(["File Name", "Confidence", "Class"])
+        writer.writerow(["File Name", "Confidence", "Class", "Image"])
         writer.writerows(_rawCSVData)
     return True
 #endregion
@@ -28,14 +29,20 @@ rawCSVData = []
 
 for result in results: 
     _imageName = os.path.basename(result.path)
-    for box in result.boxes.cpu().numpy():
+    _confidence = 0
+    _class = ''
+
+    _imageLink = '=IMAGE("' + result.path + '", "' + _imageName + '", 0)'
+
+    boxes = result.boxes.cpu().numpy()
+
+    for box in boxes:
         _confidence = str(box.conf[0]).replace(".", ",")
         _class = model.names[int(box.cls[0])]
-        print(_imageName + _class + _confidence)
 
-        rawCSVData.append((_imageName, _confidence, _class))
+        rawCSVData.append((_imageName, _confidence, _class, _imageLink))
+
+    if _confidence == 0:
+        rawCSVData.append((_imageName, _confidence, _class, _imageLink))
 
 SaveToCSV(_outputPredictionResultsPath, rawCSVData)
-
-
-
